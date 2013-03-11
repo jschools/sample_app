a17617f5f02be08d15455fd2f337c35d3d831031
Jonathan Schooler <violinplaya@gmail.com>
2013-03-10 17:46:32 -0700

    through 9.3

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 	factory :user do
-		name                  "Jonathan Schooler"
-		email                 "jschooler@ubermind.com"
-		password              "foobar"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
+		password "foobar"
 		password_confirmation "foobar"
 	end
 end
