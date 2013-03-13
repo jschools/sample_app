17bd1809dd5882478fe2b941971936e933ee4e81
Jonathan Schooler <violinplaya@gmail.com>
2013-03-13 01:32:40 -0700

    add user microposts (end of ch 10)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,52 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  subject { page }
+	subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+	describe "Home page" do
+		before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-  end
+		it { should have_selector('h1',    text: 'Sample App') }
+		it { should have_selector('title', text: full_title('')) }
 
-  describe "Help page" do
-    before { visit help_path }
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				sign_in user
+				visit root_path
+			end
 
-  	it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
-  end
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+				end
+			end
+		end
+	end
 
-  describe "About page" do
-    before { visit about_path }
+	describe "Help page" do
+		before { visit help_path }
 
-  	it { should have_selector('h1', text: 'About Us') }
-  	it { should have_selector('title', text: full_title('About Us')) }
-  end
+		it { should have_selector('h1', text: 'Help') }
+		it { should have_selector('title', text: full_title('Help')) }
+	end
 
-  describe "Contact page" do
-    before { visit contact_path }
+	describe "About page" do
+		before { visit about_path }
 
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: full_title("Contact")) }
-  end
+		it { should have_selector('h1', text: 'About Us') }
+		it { should have_selector('title', text: full_title('About Us')) }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_selector('h1', text: 'Contact') }
+		it { should have_selector('title', text: full_title("Contact")) }
+	end
 
 end
